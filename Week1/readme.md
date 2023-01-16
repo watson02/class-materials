d91dee08c038f2f45c4057e61d98ffa972334025
Aravind Mohan <aravindmohan@Aravinds-MacBook-Pro.local>
2023-01-16 17:46:49 -0500

    week1-added

---
+++ Week1/readme.md
@@ -0,0 +1,13 @@
+# In this lesson, we will learn the following:
+
+1) An Introduction to the course. 
+2) Defining an Algorithm. 
+3) Overview of the topics to be learned.
+4) Examples of Algorithms.
+
+# Week1 activity form:
+https://forms.gle/GFk87y8ZjVcscFVx6
+
+
+
+
---
+++ Week1/src/aquarium.py
@@ -0,0 +1,10 @@
+def compute():
+	# assume total 10 tanks 
+	for i in range(1,10):
+		tanks = i
+		turtles = tanks+1
+		# complete line 6 below by adding the right conditions inside the if block ...
+		if ():
+			print(turtles, tanks)
+if __name__ == "__main__":
+    compute()
