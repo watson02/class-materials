6ad69303495e9cb01306dd9b82cc90ccab5da448
Rian Watson <70479210+watson02@users.noreply.github.com>
2023-01-17 09:14:42 -0500

    Update aquarium.py

--- Week1/src/aquarium.py
+++ Week1/src/aquarium.py
@@ -4,7 +4,7 @@ def compute():
 		tanks = i
 		turtles = tanks+1
 		# complete line 6 below by adding the right conditions inside the if block ...
-		if ():
+		if (tanks > 1 and turtles/(tanks-1) == 2):
 			print(turtles, tanks)
 if __name__ == "__main__":
     compute()
